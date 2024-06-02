3c66d88c761ddba485c169d58fb06ef1608a1687
nguyenkhoa2306 <124553309+nguyenkhoa2306@users.noreply.github.com>
2024-06-02 14:53:08 +0700

    feat: cashier sidebar

--- src/components/CashierSidebar/CashierSidebar.jsx
+++ src/components/CashierSidebar/CashierSidebar.jsx
@@ -1,10 +1,48 @@
 import classes from "./CashierSidebar.module.css";
+import InvoiceList from "/assets/invoice-list.png";
+import Product from "/assets/product.png";
+import Customer from "/assets/customer.png";
+import Disount from "/assets/discount.png";
 
 const CashierSidebar = () => {
   return (
     <div className={classes.container}>
       <div className={classes.title}>
         <p>Trang thu ngân</p>
+      </div>
+      <hr />
+      <div className={classes.content}>
+        <button>
+          <img
+            src={InvoiceList}
+            alt="Invoice icon"
+            className={classes["icon"]}
+          />
+          <div className={classes["text"]}>
+            <p>Danh sách hóa đơn</p>
+          </div>
+        </button>
+
+        <button>
+          <img src={Product} alt="Product icon" className={classes["icon"]} />
+          <div className={classes["text"]}>
+            <p>Sản phẩm</p>
+          </div>
+        </button>
+
+        <button>
+          <img src={Customer} alt="Customer icon" className={classes["icon"]} />
+          <div className={classes["text"]}>
+            <p>Khách hàng</p>
+          </div>
+        </button>
+
+        <button>
+          <img src={Disount} alt="Disount icon" className={classes["icon"]} />
+          <div className={classes["text"]}>
+            <p>Khuyến mãi</p>
+          </div>
+        </button>
       </div>
       <hr />
     </div>
