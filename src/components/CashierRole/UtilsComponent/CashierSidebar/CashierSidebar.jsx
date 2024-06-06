b11aa75b9e065717fa9f99a07965ad6a38963975
minhldse181522@fpt.edu.vn <minhldse181522@fpt.edu.vn>
2024-06-06 17:54:58 +0700

    feat: add background color for current page for cashier sidebar

--- src/components/CashierRole/UtilsComponent/CashierSidebar/CashierSidebar.jsx
+++ src/components/CashierRole/UtilsComponent/CashierSidebar/CashierSidebar.jsx
@@ -3,8 +3,15 @@ import InvoiceList from "/assets/invoice-list.png";
 import Product from "/assets/product.png";
 import Customer from "/assets/customer.png";
 import Disount from "/assets/discount.png";
+import { useState } from "react";
 
 const CashierSidebar = () => {
+  const [currentPage, setCurrentPage] = useState("Danh sách hóa đơn");
+
+  const handlePage = (event) => {
+    const page = event.target.getAttribute("page");
+    setCurrentPage(page);
+  };
   return (
     <div className={classes.container}>
       <div className={classes.title}>
@@ -12,7 +19,14 @@ const CashierSidebar = () => {
       </div>
       <hr />
       <div className={classes.content}>
-        <button className={classes.button}>
+        <button
+          className={`${classes.button} ${
+            currentPage === "Danh sách hóa đơn" ? classes.current : ""
+          }`}
+          page="Danh sách hóa đơn"
+          onClick={handlePage}
+          href="/invoicelist"
+        >
           <img
             src={InvoiceList}
             alt="Invoice icon"
@@ -23,21 +37,42 @@ const CashierSidebar = () => {
           </div>
         </button>
 
-        <button className={classes.button}>
+        <button
+          className={`${classes.button} ${
+            currentPage === "Sản phẩm" ? classes.current : ""
+          }`}
+          page="Sản phẩm"
+          onClick={handlePage}
+          href="/invoicelist"
+        >
           <img src={Product} alt="Product icon" className={classes["icon"]} />
           <div className={classes["text"]}>
             <p>Sản phẩm</p>
           </div>
         </button>
 
-        <button className={classes.button}>
+        <button
+          className={`${classes.button} ${
+            currentPage === "Khách hàng" ? classes.current : ""
+          }`}
+          page="Khách hàng"
+          onClick={handlePage}
+          href="/invoicelist"
+        >
           <img src={Customer} alt="Customer icon" className={classes["icon"]} />
           <div className={classes["text"]}>
             <p>Khách hàng</p>
           </div>
         </button>
 
-        <button className={classes.button}>
+        <button
+          className={`${classes.button} ${
+            currentPage === "Khuyến mãi" ? classes.current : ""
+          }`}
+          page="Khuyến mãi"
+          onClick={handlePage}
+          href="/invoicelist"
+        >
           <img src={Disount} alt="Disount icon" className={classes["icon"]} />
           <div className={classes["text"]}>
             <p>Khuyến mãi</p>
