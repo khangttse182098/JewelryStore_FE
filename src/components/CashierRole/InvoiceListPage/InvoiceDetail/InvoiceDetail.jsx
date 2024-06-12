ccc081aa05b30c3e79b675f4e6e2aaf1a6c48c1a
khangttse182098@fpt.edu.vn <khangttse182098@fpt.edu.vn>
2024-06-12 12:44:46 +0700

    feat: add style to invoice code

--- src/components/CashierRole/InvoiceListPage/InvoiceDetail/InvoiceDetail.jsx
+++ src/components/CashierRole/InvoiceListPage/InvoiceDetail/InvoiceDetail.jsx
@@ -5,7 +5,7 @@ import classes from "./InvoiceDetail.module.css";
 import PenImg from "/assets/pen.png";
 import { formatter } from "../../../../util/formatter";
 import ImageLoader from "../../../../util/ImageLoader";
-import Skeleton from "react-loading-skeleton";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
 const InvoiceDetail = ({ invoice }) => {
   const {
@@ -71,164 +71,174 @@ const InvoiceDetail = ({ invoice }) => {
   }, []);
 
   return (
-    <div className={classes.container}>
-      <div className={classes.title}>Chi tiết hóa đơn</div>
-      <div className={classes["table-container"]}>
-        <table
-          cellspacing="0"
-          cellpadding="0"
-          className={classes["product-list"]}
-        >
-          <thead>
-            <tr className={classes["invoice-title"]}>{invoiceCode}</tr>
-            <tr className={classes["invoice-props"]}>
-              <th>Sản phẩm</th>
-              <th>Tên sản phẩm</th>
-              <th>Giá</th>
-            </tr>
-          </thead>
-          <tbody>
-            {productResponseDTOList !== null
-              ? productResponseDTOList.map((list) => {
-                  return (
-                    <tr>
-                      <td className={classes["img-container"]}>
-                        <ImageLoader
-                          URL={list.productImage}
-                          imgStyle={classes.img}
-                          skeletonStyle={classes["img-skeleton"]}
-                        />
-                      </td>
-                      <td>{list.productName}</td>
-                      <td>{formatter.format(list.price)}</td>
-                    </tr>
-                  );
-                })
-              : undefined}
-            {diamondCriteriaResponseDTOS !== null
-              ? diamondCriteriaResponseDTOS.map((list) => {
-                  return (
-                    <tr>
-                      <td className={classes["img-container"]}>
-                        {/* <img className={classes.img} src={image} alt="ring" /> */}
-                      </td>
-                      <td>Kim cương</td>
-                      <td>{formatter.format(list.price)}</td>
-                    </tr>
-                  );
-                })
-              : undefined}
-            {materialResponseDTOList !== null
-              ? materialResponseDTOList.map((list) => {
-                  return (
-                    <tr>
-                      <td className={classes["img-container"]}>
-                        {/* <img className={classes.img} src={image} alt="ring" /> */}
-                      </td>
-                      <td>Vàng</td>
-                      <td>{formatter.format(list.price)}</td>
-                    </tr>
-                  );
-                })
-              : undefined}
-          </tbody>
-        </table>
-
-        <div className={classes["customer-detail"]}>
-          <div className={classes["customer-title-container"]}>
-            <p>Khách hàng</p>
-            <div>
-              <img className={classes["img-pen"]} src={PenImg} alt="pen-logo" />
-            </div>
-          </div>
-          <hr
-            style={{ width: "419px", marginLeft: "23px", marginBottom: "10px" }}
-          />
-          <div className={classes["customer-info"]}>
-            <div className={classes["customer-name"]}>
-              <p className={classes["customer-info-title"]}>Họ và tên: </p>
-              <p>
-                {customerName || (
-                  <Skeleton style={{ width: "150px", height: "10px" }} />
-                )}
-              </p>
-            </div>
-            <div className={classes["customer-gender"]}>
-              <p className={classes["customer-info-title"]}>Giới tính: </p>
-              <p>
-                {customer.gender || (
-                  <Skeleton style={{ width: "150px", height: "10px" }} />
-                )}
-              </p>
-            </div>
-            <div className={classes["customer-phone"]}>
-              <p className={classes["customer-info-title"]}>SĐT: </p>
-              <p>
-                {customer.phoneNumber || (
-                  <Skeleton style={{ width: "150px", height: "10px" }} />
-                )}{" "}
-              </p>
-            </div>
-            <div className={classes["customer-address"]}>
-              <p className={classes["customer-info-title"]}>Địa chỉ: </p>
-              <p>
-                {customer.address || (
-                  <>
-                    <Skeleton
-                      style={{
-                        width: "100px",
-                        height: "10px",
-                      }}
-                    />
-                  </>
-                )}
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* status */}
-      <div className={classes["purchase-contatiner"]}>
-        <div className={classes["status-container"]}>
-          <div
-            className={`${
-              status === "Chưa thanh toán"
-                ? `${classes["status-inProgress"]}`
-                : `${classes["status-success"]}`
-            } ${classes.highlight}`}
+    <SkeletonTheme baseColor="#f2f2f2" highlightColor="white">
+      <div className={classes.container}>
+        <div className={classes.title}>Chi tiết hóa đơn</div>
+        <div className={classes["table-container"]}>
+          <table
+            cellspacing="0"
+            cellpadding="0"
+            className={classes["product-list"]}
           >
-            {status}
-          </div>
-          <div className={classes["status-name-containter"]}>
-            <div className={classes.highlight}>Tổng tiền hàng</div>
-            <div className={classes["status-value"]}>
-              {formatter.format(totalPrice)}
+            <thead>
+              <tr className={classes["invoice-title"]}>{invoiceCode}</tr>
+              <tr className={classes["invoice-props"]}>
+                <th>Sản phẩm</th>
+                <th>Tên sản phẩm</th>
+                <th>Giá</th>
+              </tr>
+            </thead>
+            <tbody>
+              {productResponseDTOList !== null
+                ? productResponseDTOList.map((list) => {
+                    return (
+                      <tr>
+                        <td className={classes["img-container"]}>
+                          <ImageLoader
+                            URL={list.productImage}
+                            imgStyle={classes.img}
+                            skeletonStyle={classes["img-skeleton"]}
+                          />
+                        </td>
+                        <td>{list.productName}</td>
+                        <td>{formatter.format(list.price)}</td>
+                      </tr>
+                    );
+                  })
+                : undefined}
+              {diamondCriteriaResponseDTOS !== null
+                ? diamondCriteriaResponseDTOS.map((list) => {
+                    return (
+                      <tr>
+                        <td className={classes["img-container"]}>
+                          {/* <img className={classes.img} src={image} alt="ring" /> */}
+                        </td>
+                        <td>Kim cương</td>
+                        <td>{formatter.format(list.price)}</td>
+                      </tr>
+                    );
+                  })
+                : undefined}
+              {materialResponseDTOList !== null
+                ? materialResponseDTOList.map((list) => {
+                    return (
+                      <tr>
+                        <td className={classes["img-container"]}>
+                          {/* <img className={classes.img} src={image} alt="ring" /> */}
+                        </td>
+                        <td>Vàng</td>
+                        <td>{formatter.format(list.price)}</td>
+                      </tr>
+                    );
+                  })
+                : undefined}
+            </tbody>
+          </table>
+
+          <div className={classes["customer-detail"]}>
+            <div className={classes["customer-title-container"]}>
+              <p>Khách hàng</p>
+              <div>
+                <img
+                  className={classes["img-pen"]}
+                  src={PenImg}
+                  alt="pen-logo"
+                />
+              </div>
             </div>
-          </div>
-          <div className={classes["status-name-containter"]}>
-            <div className={classes.highlight}>Khách đã trả</div>
-            <input
-              className={classes["status-value"]}
-              onChange={handleFund}
-              placeholder="Nhập tiền khách hàng trả"
-              value={payPrice}
+            <hr
+              style={{
+                width: "419px",
+                marginLeft: "23px",
+                marginBottom: "10px",
+              }}
             />
-          </div>
-          <div className={classes["status-name-containter"]}>
-            <div className={classes.highlight}>Tiền hoàn lại khách</div>
-            <div className={classes["status-value"]}>
-              {formatter.format(fund)}
+            <div className={classes["customer-info"]}>
+              <div className={classes["customer-name"]}>
+                <p className={classes["customer-info-title"]}>Họ và tên: </p>
+                <p>
+                  {customerName || (
+                    <Skeleton style={{ width: "150px", height: "10px" }} />
+                  )}
+                </p>
+              </div>
+              <div className={classes["customer-gender"]}>
+                <p className={classes["customer-info-title"]}>Giới tính: </p>
+                <p>
+                  {customer.gender || (
+                    <Skeleton style={{ width: "150px", height: "10px" }} />
+                  )}
+                </p>
+              </div>
+              <div className={classes["customer-phone"]}>
+                <p className={classes["customer-info-title"]}>SĐT: </p>
+                <p>
+                  {customer.phoneNumber || (
+                    <Skeleton style={{ width: "150px", height: "10px" }} />
+                  )}{" "}
+                </p>
+              </div>
+              <div className={classes["customer-address"]}>
+                <p className={classes["customer-info-title"]}>Địa chỉ: </p>
+                <p>
+                  {customer.address || (
+                    <>
+                      <Skeleton
+                        style={{
+                          width: "300px",
+                          height: "10px",
+                        }}
+                      />
+                    </>
+                  )}
+                </p>
+              </div>
             </div>
           </div>
         </div>
-        <button
-          className={classes["purchase-button"]}
-          onClick={handleFetchPurchase}
-        >
-          Thanh Toán
-        </button>
+        {/* status */}
+        <div className={classes["purchase-contatiner"]}>
+          <div className={classes["status-container"]}>
+            <div
+              className={`${
+                status === "Chưa thanh toán"
+                  ? `${classes["status-inProgress"]}`
+                  : `${classes["status-success"]}`
+              } ${classes.highlight}`}
+            >
+              {status}
+            </div>
+            <div className={classes["status-name-containter"]}>
+              <div className={classes.highlight}>Tổng tiền hàng</div>
+              <div className={classes["status-value"]}>
+                {formatter.format(totalPrice)}
+              </div>
+            </div>
+            <div className={classes["status-name-containter"]}>
+              <div className={classes.highlight}>Khách đã trả</div>
+              <input
+                className={classes["status-value"]}
+                onChange={handleFund}
+                placeholder="Nhập tiền khách hàng trả"
+                value={payPrice}
+              />
+            </div>
+            <div className={classes["status-name-containter"]}>
+              <div className={classes.highlight}>Tiền hoàn lại khách</div>
+              <div className={classes["status-value"]}>
+                {formatter.format(fund)}
+              </div>
+            </div>
+          </div>
+          <button
+            className={classes["purchase-button"]}
+            onClick={handleFetchPurchase}
+          >
+            Thanh Toán
+          </button>
+        </div>
       </div>
-    </div>
+    </SkeletonTheme>
   );
 };
 
