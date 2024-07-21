bd7f2d495ac980f61f008cbce0203c4a81d4f9a7
minhldse181522@fpt.edu.vn <minhldse181522@fpt.edu.vn>
2024-07-22 02:31:50 +0700

    fix: product

--- src/components/ManagerRole/ProductListPage/DeleteProduct/DeleteProduct.jsx
+++ src/components/ManagerRole/ProductListPage/DeleteProduct/DeleteProduct.jsx
@@ -1,33 +1,48 @@
 import { forwardRef, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import DoneModal from "../../../UtilComponent/DoneModal/DoneModal";
+import ErrorModal from "../../../UtilComponent/ErrorModal/ErrorModal";
 
 const DeleteProduct = forwardRef(function DeleteProduct(
   { handleHide, deleteCode },
   ref
 ) {
+  const errorModalRef = useRef();
   const doneModalRef = useRef();
   function handleOpenDoneModal() {
     doneModalRef.current.showModal();
   }
   function handleCloseDoneModal() {
     doneModalRef.current.close();
+    navigate(-1);
   }
+  function handleOpenErrorModal() {
+    errorModalRef.current.showModal();
+  }
+  function handleCLoseErrorModal() {
+    errorModalRef.current.close();
+  }
+
   const navigate = useNavigate();
   //-------------------------deleteCode---------------------
-  const handleDelete = () => {
-    fetch("http://mahika.foundation:8080/swp/api/product", {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(deleteCode),
-    })
-      .then((res) => {
+  const handleDelete = async () => {
+    try {
+      const res = await fetch("http://mahika.foundation:8080/swp/api/product", {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(deleteCode),
+      });
+      const obj = await res.json();
+      if (obj.message === "Delete product successfully") {
         handleOpenDoneModal();
-        navigate("/manager/product/list");
-      })
-      .catch((error) => console.log(error));
+      } else {
+        handleOpenErrorModal();
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -36,6 +51,11 @@ const DeleteProduct = forwardRef(function DeleteProduct(
       className="w-1/3 h-80 translate-x-full translate-y-1/2 bg-white"
     >
       <DoneModal ref={doneModalRef} handleClose={handleCloseDoneModal} />
+      <ErrorModal
+        ref={errorModalRef}
+        handleClose={handleCLoseErrorModal}
+        msg={"Không thể xóa sản phẩm trong hóa đơn"}
+      />
       <div className="flex justify-center font-medium text-2xl">
         <p className="pl-4 py-3">Thông báo</p>
       </div>
